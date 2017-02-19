8bf2a208b05514ab7549d401587d5494f40ee9ef
Joshua <joshua@joshuas-imac.local>
2017-02-19 19:52:30 +1300

    Added files and framework

---
+++ OpenEarsHeader.h
@@ -0,0 +1,22 @@
+//
+//  OpenEarsHeader.h
+//  OpenEarsCura
+//
+//  Created by Joshua on 19/02/17.
+//  Copyright © 2017 Joshua. All rights reserved.
+//
+
+
+#ifndef OpenEarsHeader_h
+#define OpenEarsHeader_h
+
+#import <OpenEars/OEPocketsphinxController.h>
+#import <OpenEars/OELanguageModelGenerator.h>
+#import <OpenEars/OEFliteController.h>
+#import <Slt/Slt.h> // Only needed if using Flite speech
+#import <OpenEars/OEEventsObserver.h>
+#import <OpenEars/OELogging.h>
+#import <OpenEars/OEAcousticModel.h>
+
+#endif /* OpenEarsHeader_h */
+
